3ce6d634eed0365c4892a28621e6f4f8dd64a11d
martinsrvinicius <73360817+martinsrvinicius@users.noreply.github.com>
2024-08-06 23:05:05 +0100

    Update 01_users.js

--- backend/src/db/seeds/01_users.js
+++ backend/src/db/seeds/01_users.js
@@ -6,8 +6,8 @@ exports.seed = async function(knex) {
     // Deletes ALL existing entries
     await knex('User').del()
     await knex('User').insert([
-      {userid: 1, name: 'Vinicius Martins',password: '$2b$10$1ZpYj0E17dmjg/X3xFGG5eLP71WsTncsvdFGO9wjutEJv4WbYWr06',email:'it@ultriplo.com',phone: '962468873',login:'vinicius',language:'br'},
-      {userid: 2, name: 'Rosemeri Martins',password: '$2b$10$1ZpYj0E17dmjg/X3xFGG5eLP71WsTncsvdFGO9wjutEJv4WbYWr06',email:'rosemeri_bock@hotmail.com',phone: '963669338',login:'rosemeri',language:'br'},
-      {userid: 3, name: 'Ana Martins',password: '$2b$10$1ZpYj0E17dmjg/X3xFGG5eLP71WsTncsvdFGO9wjutEJv4WbYWr06',email:'anagloria.apm@gmail.com',phone: '963669338',login:'ana',language:'br'},
+      {userid: 1, name: 'Vinicius Martins',password: '$2b$10$1ZpYj0E17dmjg/X3xFGG5eLP71WsTncsvdFGO9wjutEJv4WbYWr06',email:'vinim.dev@gmail.com',phone: '962468873',login:'vinicius',language:'br'},
+      {userid: 2, name: 'Rosemeri Martins',password: '$2b$10$1ZpYj0E17dmjg/X3xFGG5eLP71WsTncsvdFGO9wjutEJv4WbYWr06',email:'rosemerick@hotmail.com',phone: '984351652',login:'rosemeri',language:'br'},
+      {userid: 3, name: 'Ana Martins',password: '$2b$10$1ZpYj0E17dmjg/X3xFGG5eLP71WsTncsvdFGO9wjutEJv4WbYWr06',email:'anagloria.pm@gmail.com',phone: '95963256',login:'ana',language:'br'},
     ]);
   };
